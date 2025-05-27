3dbb101e5c9f460e926dbba315ec63c06a1e86f6
rakshabesafe <prav786@rediffmail.com>
2025-05-27 17:51:42 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 // settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
